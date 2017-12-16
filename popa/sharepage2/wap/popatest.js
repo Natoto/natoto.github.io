e0e7379241022712a699ee588cd5eceeb36dffd8
huangbo <huangbo@yy.com>
2017-12-17 00:28:05 +0800

    添加测试数据

---
+++ popa/sharepage2/wap/popatest.js
@@ -0,0 +1,65 @@
+// $(function(){
+
+
+	var POPAOPUSINFO={
+	"code": 0,
+	"data": {
+		"isNextPage": 0,
+		"userId": 0,
+		"opusList": [
+		{
+			"addressId": 0,
+			"addressImg": "",
+			"addressName": "",
+			"chatMsgCount": 0,
+			"commCount": 0,
+			"createDate": 1513155290000,
+			"id": 2160,
+			"latitude": 0,
+			"longitude": 0,
+			"mediaType": 1,
+			"msgCount": 0,
+			"opusDesc": "",
+			"opusIds": "",
+			"opusImg": "http://images.bopaiapp.com/popaimg/d/20171213165450_m1zQpFZw_1242x2208.png",
+			"opusName": "15920549038 2017-12-13 08:54:50 +0000 published",
+			"opusType": 1,
+			"praiseCount": 0,
+			"userId": 100031,
+			"userImg": "http://images.bopaiapp.com/popaimg/bg2_ppuser_1_20171120201848YVOg4diC.png",
+			"userName": "boob",
+			"videoPath": ""
+		},
+		{
+			"addressId": 245,
+			"addressImg": "http://images.bopaiapp.com/popaimg/bg0_didian22_20170806135626RansFFCJ.png",
+			"addressName": "敏捷·上城国际oo",
+			"chatMsgCount": 0,
+			"commCount": 0,
+			"createDate": 1512903274000,
+			"id": 2135,
+			"latitude": 23.002442,
+			"longitude": 113.350704,
+			"mediaType": 1,
+			"msgCount": 0,
+			"opusDesc": "",
+			"opusIds": "",
+			"opusImg": "http://images.bopaiapp.com/popaimg/d/20171210185434_7cLxzsB1_1280x2276.png",
+			"opusName": "15920549038 2017-12-10 10:54:34 +0000 published",
+			"opusType": 2,
+			"praiseCount": 0,
+			"userId": 100031,
+			"userImg": "http://images.bopaiapp.com/popaimg/bg2_ppuser_1_20171120201848YVOg4diC.png",
+			"userName": "boob",
+			"videoPath": "http://images.bopaiapp.com/loMTsiwBoTxTRXD83gIFF-HH9SaF"
+		}],
+	},
+	"msg": "",
+	"times": 1513415440095
+	}
+
+// });
+
+exports=function(){
+	return POPAOPUSINFO;
+}
